83e4900dbd02cc4537fe6ca01466fd7e64a3fead
nayf500 <nayf50023@gmail.com>
2026-08-13 17:02:20 +0300

    Add initial product list to products.js

---
+++ products.js
@@ -0,0 +1,18 @@
+const storeProducts = [
+    {
+        id: "SKU-W001",
+        name: "ساعة كلاسيكية فاخرة",
+        price: "175 ريال",
+        image: "product1.jpg",
+        category: "ساعات"
+    },
+    {
+        id: "SKU-B001",
+        name: "حقيبة نسائية جلدية",
+        price: "175 ريال",
+        image: "product2.jpg",
+        category: "حقائب"
+    }
+    // يمكنك إضافة المزيد من المنتجات هنا بنفس الطريقة:
+    // { id: "SKU-...", name: "...", price: "175 ريال", image: "...", category: "..." }
+];
